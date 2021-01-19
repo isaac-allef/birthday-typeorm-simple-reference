6ceffc53370a39f4aac13855fdc168afbb884251
Isaac Allef <isaac_allef@hotmail.com>
2021-01-19 17:11:29 -0300

    routes using custom repository and user service

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,15 +1,31 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import UserRepository from '../repositories/userRepository';
+import CreateUserService from '../services/CreateUserService';
 
 const userRoute = Router();
 
-interface UserCreate {
-    name: string,
-    age: number,
-}
+userRoute.get('/users', async (request, response) => {
+    const userRepository = getCustomRepository(UserRepository);
+    const users = await userRepository.find();
 
-userRoute.get('/users', (request, response) => {
-    const user: UserCreate = { name: 'Isaac', age: 25 };
-    response.json(user)
-})
+    return response.json(users);
+});
+
+userRoute.post('/users', async (request, response) => {
+    try {
+        const { name, birth_date } = request.body;
+
+        const createUser = new CreateUserService();
+        const user = await createUser.execute({
+            name,
+            birth_date,
+        });
+
+        response.status(201).json(user);
+    } catch(err) {
+        response.status(400).json({ error: err.message });
+    }
+});
 
  export default userRoute;
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import './database';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(router);
 
 app.listen(3333, () => console.log('It\'s runing'));
