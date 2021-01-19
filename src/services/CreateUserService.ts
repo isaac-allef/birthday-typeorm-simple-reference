3d56eb322d182135375ed6a408ed99d8ae62eb7d
Isaac Allef <isaac_allef@hotmail.com>
2021-01-19 17:06:55 -0300

    user service error handling

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -9,6 +9,11 @@ interface RequestDTO {
 
 class CreateUserService {
     public async execute({ name, birth_date }: RequestDTO): Promise<User> {
+        const birth_date_year = birth_date.getFullYear();
+        if (birth_date_year < 1920) {
+            throw Error('This birth date is from someone very very old');
+        }
+
         const userRepository = getCustomRepository(UserRepository);
         const user = userRepository.create({ name, birth_date });
         await userRepository.save(user);
