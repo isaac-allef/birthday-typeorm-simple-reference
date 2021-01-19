294546a454fc2e5f79eb4bc9cc68b57a6082654e
Isaac Allef <isaac_allef@hotmail.com>
2021-01-19 15:37:58 -0300

    migration config and creation

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Steps to run
 - Create postgres docker: sudo docker run --name user-postgres -e POSTGRES_PASSWORD=123 -p 5434:5432 -d postgres
 - Create 'users' database
-...
+- Run migrations: yarn typeorm migration:run
---
+++ src/database/migrations/1611079976085-CreateUsers.ts
@@ -0,0 +1,35 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateUsers1611079976085 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'birth_date',
+                        type: 'date',
+                        isNullable: false,
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+
+}
